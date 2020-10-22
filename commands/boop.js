1cddb90f18339c1b9b1174b9dce7bf6d50486d62
Julie Wojtiw-Quo <wojtiwquoj@uleth.ca>
2020-10-22 02:31:02 -0600

    Boop

---
+++ commands/boop.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'Boop',
+	description: 'Tag a member and boop them (but not really).',
+	execute(message) {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to boop them!');
+		}
+
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to boop: ${taggedUser.username}`);
+	},
+};
