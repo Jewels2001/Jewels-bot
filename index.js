95d83e286939ba36ec61a02dfb1f84613011ade9
Julie Wojtiw-Quo <wojtiwquoj@uleth.ca>
2020-10-22 01:54:32 -0600

    First commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('message', msg => {
   //event listeners callback function
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   //If does not start with prefix OR sent by a bot, exit.
-  const args = msg.content.slice(prefix.length).trim().split(' ');
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
   //Slices off prefix, removes whitespace, splits into array by spaces
   const command = args.shift().toLowerCase();
   //Takes first element in array and removes it.
