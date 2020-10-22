b9bcb86dc5650f5517093439d6fe130b787ca1d9
Julie Wojtiw-Quo <wojtiwquoj@uleth.ca>
2020-10-22 01:26:37 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 //Good copy of index.js / what we want the bot to do
 
 const Discord = require("discord.js");
-const { prefix, tokem } = require("./config.json");
-
-
+const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
 
 
-client.on('ready', () => {
+
+client.once('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
 });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
+    msg.channel.send('Polo.');
   }
 });
 
-client.login('token');
+client.login(token);
