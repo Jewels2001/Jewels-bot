9c6604450c71e7fa83164214bbbd89b02e948aae
Julie Wojtiw-Quo <wojtiwquoj@uleth.ca>
2020-10-22 00:41:55 -0600

    Initial commit

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 
 const Discord = require("discord.js");
 const { prefix, tokem } = require("./config.json");
+
+
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('token');
