06de924883458c40f4e09c41a45e36e3515b5329
Julie Wojtiw-Quo <wojtiwquoj@uleth.ca>
2020-11-09 03:51:28 -0700

    Dyanmic Commands

--- commands/args-info.js
+++ commands/args-info.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'args-info',
 	description: 'Information about arguments provided.',
 	execute(message, args) {
-		if(!args.length{
+		if(!args.length){
       return message.channel.send(`You didn't provide any argumemts, ${message.author}!`);
     }
     else i(args[0] === 'foo'){
